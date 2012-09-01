4b09f31e7f257e7335ce46b0d180dcd832316c72
Christophe Knage <cvknage@gmail.com>
2012-09-01 13:44:41 +0200

    Initial Commit

---
+++ BlockCategories/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlockCategories
+//
+//  Created by Christophe Knage on 01/09/2012.
+//  Copyright (c) 2012 Christophe Knage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
