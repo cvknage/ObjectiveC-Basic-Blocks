e92c977d9780181829c29dbfe37bfb993a16e7b9
Christophe Knage <cvknage@gmail.com>
2013-01-26 14:47:40 +0100

    Added missing header documentation

--- Classes/ClassExtensions/UIAlertView/UIAlertView+Blocks.h
+++ Classes/ClassExtensions/UIAlertView/UIAlertView+Blocks.h
@@ -19,6 +19,7 @@
  * @param message:      The message of the UIAlertView.
  * @param cancleButton: An instance of UIAlertButton to specefy the cancle button.
  * @param otherButtons: A nil terminated list of UIAlertButton(s).
+ * @return Newly initialized alert view (ready to be shown).
  */
 - (id)initWithTitle:(NSString *)title message:(NSString *)message cancleButton:(UIAlertButton *)cancleButton otherButtons:(UIAlertButton *)otherButton, ... NS_REQUIRES_NIL_TERMINATION;
 
@@ -26,6 +27,7 @@
  * Add a UIAlertButton to the UIAlertView.
  *
  * @param button: An instance of UIAlertButton to specefy the button to add.
+ * @return The index of the new button. Button indices start at 0 and increase in the order they are added.
  */
 - (NSInteger)addButton:(UIAlertButton *)button;
 
